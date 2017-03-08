64929041543d70da201cbaca12ced6f224f4b41e
vgvenkat <vgkrish619@gmail.com>
2017-03-08 14:25:52 -0800

    Filtering coffee hole ☕️

--- js/app.js
+++ js/app.js
@@ -1,14 +1,30 @@
-$(function(){
-    ko.applyBindings(ViewModel);
-    ViewModel.loadCoffeeList()
-})
-var ViewModel = {
-    coffeeList : ko.observableArray([]),
-    loadCoffeeList : function() {
+var ViewModel = function(){
+    this.coffeeList = ko.observableArray([]);
+    this.filteredCoffeeList = ko.observableArray([]);
+    this.coffeeInput = ko.observable('');
+   
         var self = this;
          $.getJSON("js/model.json", function(data){
             self.coffeeList(data);
+        }).done(function(){
+            self.filterInput();
         })
-    }
-}
+    
+    this.filterInput = function() {
+        self.filteredCoffeeList.removeAll();
+        var entry = self.coffeeInput().toLowerCase();
+        console.log(entry.length,self.coffeeList())
+        ko.utils.arrayFilter(self.coffeeList(), function(item) {
+            var isThere = item.name.toLowerCase().indexOf(entry)
+            if(isThere>=0) {
+                self.filteredCoffeeList.push(item);
+            }
+        
+        })
 
+       
+    }
+    
+    this.coffeeInput.subscribe(this.filterInput)
+}
+ko.applyBindings(new ViewModel());
