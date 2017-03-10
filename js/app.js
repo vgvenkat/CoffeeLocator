fc2083ec8717fa9a0e777167ac56fbcaaf5f4dbf
vgvenkat <vgkrish619@gmail.com>
2017-03-10 12:33:06 -0800

    Almost there ✈️

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
 var CLIENT_ID='QER25ZOFOI5NZUNVJZOFB4YJFCLGYHB51FTIWFO2IOHTRWS3'
 var CLIENT_SECRET='TNXWNKHS1CXIK1VPPEYIYDJVCE0J0Z3AYMYBZSKWVS4A1AIC'
-var URL= "https://api.foursquare.com/v2/venues/explore?client_id="+CLIENT_ID+"&client_secret="+CLIENT_SECRET+"&near=San Francisco,CA&section=food&limit=10&v=20170309&query=coffee"
+var URL= "https://api.foursquare.com/v2/venues/explore?client_id="+CLIENT_ID+"&client_secret="+CLIENT_SECRET+"&near=San Francisco,CA&section=food&limit=10&v=20170309&query=coffee&venuePhotos=1"
 var map;
+var contentString = "<div class='infoWindow'>"+
+                    "<div class='left'><img src='%imgSource%'/></div>"+
+                    "<div class='right'>"+
+                    "<h3>%locationName%</h3>"+
+                    "<p>%address%</p>"+
+                    "<span>Stars: %stars%</span>"+
+                    "</div></div>";
 function initMap() {
     var SFO = {
         lat: 37.7749,
         lng: -122.4194
     };
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
+        zoom: 13,
         center: SFO
 
     });
@@ -21,23 +28,36 @@ function initMap() {
 
     
 }
-function toggleBounce(marker) {
-        if (marker.getAnimation() !== null) {
-            marker.setAnimation(null);
+function toggleBounceAndInfo(marker) {
+        if (marker.marker.getAnimation() !== null) {
+            marker.marker.setAnimation(null);
+            marker.infowindow.close(); 
         } else {
-            marker.setAnimation(google.maps.Animation.BOUNCE);
-            setTimeout(function(){marker.setAnimation(null)}, 5000);
+            marker.marker.setAnimation(google.maps.Animation.BOUNCE);
+
+            marker.infowindow.open(map, marker.marker);
+            setTimeout(function(){
+                marker.marker.setAnimation(null);
+                marker.infowindow.close(); 
+            }, 5000);
         }
     }
 
-var Marker = function(lat, lng, timeout) {
-    window.setTimeout(function(){
-  this.marker =  new google.maps.Marker({
+var Marker = function(lat, lng, name, address, rating, photoUrl) {
+    this.name = name; 
+    this.marker =  new google.maps.Marker({
                 position:{lat: lat, lng: lng},
                 map: map,
                 animation: google.maps.Animation.DROP
-            })
-            }, timeout)
+            });
+    var displayWindow = contentString.replace("%locationName%", name)
+    .replace("%address%",address )
+    .replace("%stars%", rating)
+    .replace("%imgSource%", photoUrl)
+    this.infowindow = new google.maps.InfoWindow({
+        content: displayWindow
+    })
+      
 }
 var coffeeData = '';
     
@@ -58,20 +78,39 @@ var ViewModel = function () {
             coffeeData = data.response.groups[0].items;
             self.coffeeList(coffeeData);
             coffeeData.map(function(item, index){
-                
-            var marker = new Marker(item.venue.location.lat, item.venue.location.lng, index*200);
-            
-            self.markerList.push(marker.marker)
+                var venue = item.venue;
+                var photoSize = "40x40";
+                var photos = venue.featuredPhotos.items[0];
+                var photoUrl = photos.prefix+photoSize+photos.suffix;
+                var location = venue.location;
+                window.setTimeout(function(){
+                    var marker = new Marker(location.lat, location.lng,venue.name, location.address, venue.rating, photoUrl);
+             google.maps.event.addListener(marker.marker, 'click', function() {
+                   toggleBounceAndInfo(marker);
+                });
+            self.markerList.push(marker)
+                }, index*200)
+           
             })
         }).fail(function(error){
             console.log(error)
         }).always(function(){
             self.filterInput();
         })
+        // Selecting location and stopping animation for the previous location.
+    this.selectLocation = function(marker) {
+        // if (self.selectedLocation() != 'Dummy' && self.selectedLocation() != location) {
+            toggleBounce(marker.marker);
+        //     self.selectedLocation().marker.info.close();
+        // }
+        // location.marker.info.open(map, location.marker);
+        // if (self.selectedLocation() != location)
+        //     toggleBounce(location.marker);
+        // self.selectedLocation(location);
+    };
     this.filterInput = function () {
         self.filteredCoffeeList.removeAll();
         var entry = self.coffeeInput().toLowerCase();
-        console.log(entry.length, self.coffeeList())
         ko.utils.arrayFilter(self.coffeeList(), function (item) {
             itemName = item.venue.name
             if(itemName.length>0) itemName = itemName.toLowerCase()
@@ -81,6 +120,11 @@ var ViewModel = function () {
             }
 
         })
+        ko.utils.arrayFilter(self.markerList(), function(marker) {
+            var contains = marker.name.toLowerCase().indexOf(entry) >= 0;
+            marker.marker.setVisible(contains);
+            
+        });
 
 
     }
