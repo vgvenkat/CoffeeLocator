653303ad2e44c3b8e4e6a528fb6172b69c2f97ef
vgvenkat <vgkrish619@gmail.com>
2017-03-09 16:50:37 -0800

    Little Progress

--- js/app.js
+++ js/app.js
@@ -1,38 +1,51 @@
 var CLIENT_ID='QER25ZOFOI5NZUNVJZOFB4YJFCLGYHB51FTIWFO2IOHTRWS3'
 var CLIENT_SECRET='TNXWNKHS1CXIK1VPPEYIYDJVCE0J0Z3AYMYBZSKWVS4A1AIC'
 var URL= "https://api.foursquare.com/v2/venues/explore?client_id="+CLIENT_ID+"&client_secret="+CLIENT_SECRET+"&near=San Francisco,CA&section=food&limit=10&v=20170309&query=coffee"
-
+var map;
 function initMap() {
     var SFO = {
         lat: 37.7749,
         lng: -122.4194
     };
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
         center: SFO
 
     });
-    var marker = new google.maps.Marker({
-        position: SFO,
-        map: map,
-        animation: google.maps.Animation.DROP
-    });
-    marker.addListener('click', toggleBounce);
+    // var marker = new google.maps.Marker({
+    //     position: SFO,
+    //     map: map,
+    //     animation: google.maps.Animation.DROP
+    // });
+    // marker.addListener('click', toggleBounce);
 
-    function toggleBounce() {
+    
+}
+function toggleBounce(marker) {
         if (marker.getAnimation() !== null) {
             marker.setAnimation(null);
         } else {
             marker.setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(function(){marker.setAnimation(null)}, 5000);
         }
     }
+
+var Marker = function(lat, lng, timeout) {
+    window.setTimeout(function(){
+  this.marker =  new google.maps.Marker({
+                position:{lat: lat, lng: lng},
+                map: map,
+                animation: google.maps.Animation.DROP
+            })
+            }, timeout)
 }
 var coffeeData = '';
+    
 var ViewModel = function () {
     this.coffeeList = ko.observableArray([]);
     this.filteredCoffeeList = ko.observableArray([]);
     this.coffeeInput = ko.observable('');
-
+    this.markerList = ko.observableArray([]);
     var self = this;
     // $.getJSON("js/model.json", function (data) {
     //     self.coffeeList(data);
@@ -44,6 +57,12 @@ var ViewModel = function () {
         }).done(function(data){
             coffeeData = data.response.groups[0].items;
             self.coffeeList(coffeeData);
+            coffeeData.map(function(item, index){
+                
+            var marker = new Marker(item.venue.location.lat, item.venue.location.lng, index*200);
+            
+            self.markerList.push(marker.marker)
+            })
         }).fail(function(error){
             console.log(error)
         }).always(function(){
